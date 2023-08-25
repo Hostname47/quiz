f393ce1d468abdbca178a86695c66ee8cd23d2f1
HOSTNAME47 <mouadstev1@gmail.com>
2023-08-25 15:50:17 +0100

    Connect the app with redux by providing store to the whole app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import {Provider} from 'react-redux';
+import store from './src/app/store';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <StatusBar />
-    </SafeAreaView>
+    <Provider store={store}>
+      <SafeAreaView>
+        <StatusBar backgroundColor="#191b1e" />
+      </SafeAreaView>
+    </Provider>
   );
 }
 
