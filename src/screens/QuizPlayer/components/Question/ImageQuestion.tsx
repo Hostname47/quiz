fe8b4201962fbe0802dae99a97d20f95389a0688
HOSTNAME47 <mouadstev1@gmail.com>
2023-09-05 16:36:30 +0100

    Handle question with image by showing image

--- src/screens/QuizPlayer/components/Question/ImageQuestion.tsx
+++ src/screens/QuizPlayer/components/Question/ImageQuestion.tsx
@@ -1,11 +1,13 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Image, View} from 'react-native';
 import React from 'react';
 import {ImageQuiz} from '../../../../utils/types';
 
 const ImageQuestion = ({quiz}: {quiz: ImageQuiz}) => {
   return (
     <View style={styles.container}>
-      <Text>ImageQuestion</Text>
+      <View style={styles.imageContainer}>
+        <Image source={quiz.image} style={styles.image} />
+      </View>
     </View>
   );
 };
@@ -15,5 +17,16 @@ export default ImageQuestion;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  imageContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    width: '100%',
+  },
+  image: {
+    resizeMode: 'contain',
+    height: '100%',
+    width: '100%',
   },
 });
